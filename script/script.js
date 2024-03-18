9c8e43edcb29c170326e730ef8f8bd81521f1c09
Andraina Ho Kiady <ahkiady@pm.me>
2024-03-18 13:41:23 +0100

    Update

--- script/script.js
+++ script/script.js
@@ -1,2 +1,13 @@
-let desc = document.getElementById("desc");
-desc.innerText = "Hello, world!";
+let canvas = document.querySelector("#canvas");
+let stuff = document.querySelector("html");
+let context = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+context.fillStyle = "black";
+context.fillRect(0, 0, canvas.width, canvas.height)
+canvas.addEventListener("mousemove", e => {
+    context.fillStyle = "#5102fc";
+    context.beginPath();
+    context.arc(e.offsetX, e.offsetY, 25, 0, Math.PI * 2, false);
+    context.fill();
+})
