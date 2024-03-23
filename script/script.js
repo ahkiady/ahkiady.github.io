51518bd3528db5373cb7e6648dd8b71abd451971
Andraina Ho Kiady <ahkiady@pm.me>
2024-03-23 02:29:39 +0100

    Update

--- script/script.js
+++ script/script.js
@@ -1,38 +1,47 @@
-function quoteSomeStuffs() {
-    let lyrics = [
-        "La La Love You.",
-        "Ground Control to Major Tom.",
-        "Shake down 1979.",
-        "If you're gonna scream, scream with me.",
-        "Mesmerize.",
-        "I'm on the stereo.",
-        "Boys don't cry.",
-        "風をあつめて",
-        "Poupée de cellophane, cheveux chinois.",
-        "Crazy rides rocket, who has a magic wand.",
-        "Close my eyes, feel me now.",
-        "Matières sensuelles et sans suites.",
-        "And thank your God that I'm not aware.",
-        "I love you, metallic blue.",
-        "Glissant du bout de mes doigts, le sable.",
-        "In dreams, I'm moving through heavy water.",
-        "Goodbye horses.",
-        "You and me in time.",
-        "Jai guru deva, om.",
-        "We love the jams, and jams run free.",
-        "Your confusion, my illusion.",
-        "And you don't seem to understand.",
-        "The Thunderclouds broke up.",
-        "I cannot translate, Japanese to English.",
-        "You dance like the June Sky.",
-        "I'm dying. Is it blissful ?",
-        "Words are just noise.",
-        "In hourglass, grain of sand"
+function moveAround(e, x, y){
+    let move = [
+        {transform: "translate(0%)"},
+        {transform: `translate(${x}px, ${y}px)`},
     ];
-    let quote = document.getElementById("quotes");
-    quote.innerHTML = lyrics[Math.floor(Math.random() * lyrics.length)].toLowerCase();
+    let timing = {
+        duration: 500,
+        fill: "forwards",
+    }
+    let elem = document.querySelector(e);
+
+    let animation = elem.animate(move, timing);
+    elem.addEventListener("animationend", async (event) => {
+
+        await animation.finished;
+
+        animation.commitStyles();
+    });
+    return animation;
 }
 
-function run() {
-    setInterval(quoteSomeStuffs, 10*1000);
+function scaleDown(e, sizeX, sizeY) {
+    let scale = [
+        {transform: "scale(100%, 100%"},
+        {transform: `scale(${sizeX}, ${sizeY})`}
+    ];
+    let timing = {
+        duration: 500,
+        fill: "forwards",
+    }
+    let elem = document.querySelector(e);
+
+    let animation = elem.animate(scale, timing);
+    elem.addEventListener("animationend", async (event) => {
+
+        await animation.finished;
+
+        animation.commitStyles();
+    });
+    return animation;
+}
+
+function pictureMove() {
+    scaleDown("#pfp", 0.75, 0.75);
+    moveAround("#pfp", 0, 250);
+    moveAround("#title", 0, 650);
 }
